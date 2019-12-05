6d5da9fca5f340629117f102ac4a605bcf9087e1
XeniaSchamne <44459946+XeniaSchamne@users.noreply.github.com>
2019-12-05 01:31:03 +0100

    test

--- app/src/main/java/com/example/dontyoudare/MainActivity.java
+++ app/src/main/java/com/example/dontyoudare/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity{
                     }
                     else{
                         openCamera();
+                        
                     }
                 }
                 else{
