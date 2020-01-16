c613fc15978f138f47df49e90bf061944fb02ec0
MarcoHoeffler <44459976+MarcoHoeffler@users.noreply.github.com>
2020-01-16 21:33:05 +0100

    Animation für gelöschte Elemente in der Liste + Listenvergleich(Background )
    Liste wird nicht mehr komplett erzeugt, sondern die Elemente werden verglichen und dann wird die Liste upgedatet => Code wird eingespart und geringerer Rehenaufwand
    
    Co-Authored-By: xeniaschamne <xeniaschamne@users.noreply.github.com>

--- app/src/main/java/com/example/dontyoudare/Me.java
+++ app/src/main/java/com/example/dontyoudare/Me.java
@@ -80,7 +80,7 @@ public class Me extends Fragment {
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(@Nullable List<Note> notes) {
-                adapter.setNotes(notes);
+                adapter.submitList(notes);
 
             }
         });
